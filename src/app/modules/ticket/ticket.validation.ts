857a90966cf8cec0e8a10a1c3a0ad0b6ca0f6472
Sazidul Alam <sazidulalam20@gmail.com>
2025-05-23 18:29:30 +0600

    added date validations

--- src/app/modules/ticket/ticket.validation.ts
+++ src/app/modules/ticket/ticket.validation.ts
@@ -1,9 +1,29 @@
+import { DateTime } from 'luxon';
 import { z } from 'zod';
 
 const searchTickets = z.object({
     from: z.string(),
     to: z.string(),
-    date: z.string().date(),
+    date: z
+        .string()
+        .date()
+        .refine(
+            (value) => {
+                const date = DateTime.fromISO(value, {
+                    zone: 'Asia/Dhaka',
+                }).startOf('day');
+                const today = DateTime.now()
+                    .setZone('Asia/Dhaka')
+                    .startOf('day');
+                const maxDate = today.plus({ days: 10 });
+
+                return date >= today && date <= maxDate;
+            },
+            {
+                message:
+                    'Date must be between today and 10 days from now (Asia/Dhaka)',
+            },
+        ),
 });
 
 export const TicketValidations = {
