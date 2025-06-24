f0afa92e42019d05e670bc9105c73a078297ee79
Sazidul Alam <sazidulalam20@gmail.com>
2025-06-24 21:43:42 +0600

    remove unused files and clean up the codebase

--- dist/app/utils/formatStationNameShohoz.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const formatStationNameShohoz = (station) => {
-    return station.trim().replace(/ /g, '%20');
-};
-exports.default = formatStationNameShohoz;
